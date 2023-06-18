2f7b865a98acb2413891c62fe09f4e036a15f8e5
Maruf Robin <marufrobin5@gmail.com>
2023-06-18 09:52:29 +0600

    shimmer effect done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,15 +24,16 @@ class _MyHomePageState extends State<MyHomePage> {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: const Color(0xffF1F5FA),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Shimmer.fromColors(
-            child: Text(
+            baseColor: Colors.lightBlue,
+            highlightColor: Colors.black,
+            child: const Text(
               "Shimmer testing",
               style: TextStyle(fontSize: 30),
-            ),
-            baseColor: Colors.lightBlue,
-            highlightColor: Colors.black),
+            )),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -46,94 +47,96 @@ class _MyHomePageState extends State<MyHomePage> {
                 '$_counter',
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
-              Column(children: [
-                Container(
-                  height: height,
-                  width: width,
-                  // color: Colors.black,
-                  child: ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: 10,
-                    itemBuilder: (context, index) => Container(
-                      margin: EdgeInsets.symmetric(vertical: height * 0.016),
-                      padding: EdgeInsets.symmetric(
-                          horizontal: width * 0.0014,
-                          vertical: height * 0.0016),
-                      // height: height * 0.088,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Color.fromRGBO(32, 79, 200, 0.07),
-                              offset: Offset(0, 6),
-                              blurRadius: 15,
-                            ),
-                          ]),
-                      child: ListTile(
-                        leading: Shimmer.fromColors(
-                          baseColor: Colors.grey.shade300,
-                          highlightColor: Colors.grey.shade100,
-                          enabled: true,
-                          child: Container(
-                            // height: height * 0.054,
+              Container(
+                height: height,
+                width: width,
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                // color: Colors.lightBlue.shade50,
+                child: ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) => Container(
+                    margin: EdgeInsets.symmetric(vertical: height * 0.016),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: width * 0.0014, vertical: height * 0.0016),
+                    // height: height * 0.088,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color.fromRGBO(32, 79, 200, 0.07),
+                            offset: Offset(0, 6),
+                            blurRadius: 15,
+                          ),
+                        ]),
+                    child: ListTile(
+                      leading: Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        enabled: true,
+                        child: Container(
+                          // height: height * 0.054,
 
-                            padding: EdgeInsets.only(
-                              left: width * 0.008,
-                              right: width * 0.022,
-                              top: height * 0.018,
-                              bottom: height * 0.018,
-                            ),
-                            decoration: BoxDecoration(
-                                color: Color(0xffCDEFFF),
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Container(
-                              width: 40,
-                              height: 30,
+                          padding: EdgeInsets.only(
+                            left: width * 0.008,
+                            right: width * 0.022,
+                            top: height * 0.018,
+                            bottom: height * 0.018,
+                          ),
+                          decoration: BoxDecoration(
+                              color: const Color(0xffCDEFFF),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Container(
+                            width: 40,
+                            height: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      title: Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        enabled: true,
+                        child: Container(
+                          width: 30,
+                          height: 10,
+                          decoration: BoxDecoration(
                               color: Colors.white,
-                            ),
-                          ),
+                              borderRadius: BorderRadius.circular(4)),
+                          margin: EdgeInsets.only(right: width * 0.24),
                         ),
-                        title: Shimmer.fromColors(
-                          child: Container(
-                            width: 30,
-                            height: 10,
-                            color: Colors.white,
-                            margin: EdgeInsets.only(right: width * 0.4),
-                          ),
-                          baseColor: Colors.grey.shade300,
-                          highlightColor: Colors.grey.shade100,
-                          enabled: true,
+                      ),
+                      subtitle: Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        enabled: true,
+                        child: Container(
+                          width: double.infinity,
+                          height: 10.0,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(4)),
+                          margin: EdgeInsets.only(right: width * 0.1),
                         ),
-                        subtitle: Shimmer.fromColors(
-                          baseColor: Colors.grey.shade400,
-                          highlightColor: Colors.grey.shade300,
-                          enabled: true,
-                          child: Container(
-                            width: double.infinity,
-                            height: 10.0,
-                            color: Colors.white,
-                            margin: EdgeInsets.only(right: width * 0.2),
-                          ),
-                        ),
-                        trailing: Shimmer.fromColors(
-                          baseColor: Colors.grey.shade400,
-                          highlightColor: Colors.grey.shade300,
-                          enabled: true,
-                          child: Container(
-                            width: 80,
-                            height: 16,
-                            padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
-                            decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.circular(28)),
-                          ),
+                      ),
+                      trailing: Shimmer.fromColors(
+                        baseColor: Colors.grey.shade300,
+                        highlightColor: Colors.grey.shade100,
+                        enabled: true,
+                        child: Container(
+                          width: 80,
+                          height: 16,
+                          padding: const EdgeInsets.fromLTRB(10, 2, 10, 2),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(28)),
                         ),
                       ),
                     ),
                   ),
-                )
-              ])
+                ),
+              )
             ],
           ),
         ),
